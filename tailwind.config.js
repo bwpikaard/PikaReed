30904411de47db905c2553f7ee505c91793c5de9
DrMonotone <calabriley@yahoo.com>
2023-02-13 10:40:55 -0600

    added additional content to .exports

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,8 @@
  */
 module.exports = {
   content: [
-    "./node_modules/flowbite-react/**/*.js",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
     "./pages/**/*.{ts,tsx}",
     "./public/**/*.html",
   ],
