c1cb455a25a77edd45ff3c41873900caf96d2d37
DrMonotone <calabriley@yahoo.com>
2023-02-18 14:45:15 -0600

    added content for flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@
  */
 module.exports = {
     content: [
+        "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
         "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
         "./src/**/*.{js,jsx,ts,tsx}",
         "./pages/**/*.{ts,tsx}",
