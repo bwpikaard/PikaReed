becc425d84b74820aa886fd90a14f7645a26d6d4
Bryan Pikaard <bryan@pikaard.com>
2023-02-18 07:40:38 +0000

    db / config

---
+++ ormconfig.cjs
@@ -0,0 +1,15 @@
+import { DataSource } from "typeorm";
+import config from "config";
+
+export default new DataSource({
+    type: "postgres",
+    host: config.get("database.host"),
+    port: config.get("database.port"),
+    username: config.get("database.username"),
+    password: config.get("database.password"),
+    database: config.get("database.database"),
+    schema: "public",
+    entities: ["src/entities/**/*.entity.ts"],
+    migrationsTableName: "migrations",
+    migrations: ["migrations/*.ts"],
+})
