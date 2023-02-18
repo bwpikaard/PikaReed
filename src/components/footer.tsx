1feaa28bea4f525d9000be0600e7599bb4ad59b8
DrMonotone <calabriley@yahoo.com>
2023-02-18 13:36:45 -0600

    Changed footer to default flowbite

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,32 +1,27 @@
-import {
-    Footer as FlowbiteFooter,
-} from "flowbite-react";
+import Link from "next/link";
 import type {ReactElement} from "react";
 
 export default function Footer(): ReactElement {
     return (
-        <FlowbiteFooter
-            container={true}
-            className="pt-10 mt-auto"
-        >
-            <FlowbiteFooter.Copyright
-                href="/"
-                by="PikaReed™"
-                year={2023} />
-            <FlowbiteFooter.LinkGroup>
-                <FlowbiteFooter.Link href="#">
-                    About
-                </FlowbiteFooter.Link>
-                <FlowbiteFooter.Link href="#">
-                    Privacy Policy
-                </FlowbiteFooter.Link>
-                <FlowbiteFooter.Link href="#">
-                    Licensing
-                </FlowbiteFooter.Link>
-                <FlowbiteFooter.Link href="#">
-                    Contact
-                </FlowbiteFooter.Link>
-            </FlowbiteFooter.LinkGroup>
-        </FlowbiteFooter>
+        
+        <footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800">
+            <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <Link href="/" className="hover:underline">PikaReed™</Link>. All Rights Reserved.
+            </span>
+            <ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+                <li>
+                    <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+                </li>
+                <li>
+                    <a href="#" className="mr-4 hover:underline md:mr-6">Privacy Policy</a>
+                </li>
+                <li>
+                    <a href="#" className="mr-4 hover:underline md:mr-6">Licensing</a>
+                </li>
+                <li>
+                    <a href="#" className="hover:underline">Contact</a>
+                </li>
+            </ul>
+        </footer>
+
     );
 }
