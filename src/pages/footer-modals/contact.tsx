cb94bdaf5f6208becc09f24cda0ae45355ad6dd1
DrMonotone <calabriley@yahoo.com>
2023-02-22 22:56:14 -0600

    added contact modal page

---
+++ src/pages/footer-modals/contact.tsx
@@ -0,0 +1,20 @@
+import type {ReactElement} from "react";
+
+export default function Contact(): ReactElement {
+    return (
+        <>
+            <pre>
+                
+                Phone: (XXX)XXX-XXXX<br/>
+                <br/>
+                Email: creeder3@my.apsu.edu<br/>
+                       bpikaard@my.apsu.edu<br/>
+                <br/>
+                Office: 601 College St,<br/>
+                        Clarksville, TN<br/>
+                        37044<br/>
+
+            </pre>
+        </>
+    );
+}
