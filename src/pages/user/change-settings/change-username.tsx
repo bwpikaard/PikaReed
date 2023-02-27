e9019ece516004d91544d46a902de2808af26eb0
DrMonotone <calabriley@yahoo.com>
2023-02-27 01:18:21 -0600

    added change username to user settings

---
+++ src/pages/user/change-settings/change-username.tsx
@@ -0,0 +1,16 @@
+import type {ReactElement} from "react";
+
+export default function ChangeAvatar(): ReactElement {
+    return (
+        <>
+            <div className="mb-6">
+                <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">New Username</label>
+                <input type="text" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            </div>
+            <div className="mb-6">
+                <label htmlFor="default-input" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm New Username</label>
+                <input type="text" id="default-input" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+            </div>
+        </>
+    );
+}
