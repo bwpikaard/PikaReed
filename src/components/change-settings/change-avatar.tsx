b0f1ebd37b6c5da1f29038bc3d7528be687b9505
DrMonotone <123600653+DrMonotone@users.noreply.github.com>
2023-03-26 22:19:52 -0500

    moved change-settings to components to remove path

--- src/components/change-settings/change-avatar.tsx
+++ src/components/change-settings/change-avatar.tsx
@@ -5,12 +5,13 @@ export default function ChangeAvatar(): ReactElement {
     return (
         <>
             
-            <Image className="rounded-full w-96 h-96" src="/no-pfp.ico" alt="image description" />
+            <Image className="rounded-full w-96 h-96" src="/no-pfp.ico" alt="image description" height="300" width="300"/>
 
             <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="file_input">Upload file</label>
             <input className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="file_input_help" id="file_input" type="file" />
-            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, JPG or GIF (MAX. 800x400px).</p>
-
+            <p className="mt-1 text-sm text-gray-500 dark:text-gray-300" id="file_input_help">SVG, PNG, or JPG (MAX. 200x200px).</p>
+            
+            <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </>
     );
 }
