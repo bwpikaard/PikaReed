ff7e1fb6ffac341955de499f00ca3d337fa0d1a3
DrMonotone <calabriley@yahoo.com>
2023-04-17 01:20:23 -0500

    added mock privacy policy

--- src/components/footer-modals/privacy-policy.tsx
+++ src/components/footer-modals/privacy-policy.tsx
@@ -2,9 +2,14 @@ import type {ReactElement} from "react";
 
 export default function PrivacyPolicy(): ReactElement {
     return (
-        <pre>
-            We do so solemly swear not to steal your credit card information... <br />
-            This will be another area to look into
-        </pre>
+        <div>
+            In order to function, the only information the site collects is a user&apos;s email.<br/><br/>
+            The information is gathered at the time of account creation, and solely used for communication and login purposes.<br/><br/>
+            User data is not shared with any third-party services, and the information that is gathered has been encrypted for protection.<br/><br/>
+            The gathered data will be retained until a user requests to delete their account, upon which all stored data will be purged.<br/><br/>
+            If there are any additional concerns about the data gathered on users, requests can be made to staff in order to gain access, correct, or delete unnecessary stored information.<br/><br/>
+            While the website is deisgned to be family friendly, it should be noted that there are certain subjects which may be too violent for children.<br/><br/>
+            Any changes made to the privacy policy will announced as soon as possible.
+        </div>
     );
 }
