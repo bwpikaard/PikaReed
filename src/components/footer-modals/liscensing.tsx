3feac5be5f89ad91fe64a84f65306b49a9621348
DrMonotone <calabriley@yahoo.com>
2023-04-17 01:20:13 -0500

    properly sourced all frameworks and libraries

--- src/components/footer-modals/liscensing.tsx
+++ src/components/footer-modals/liscensing.tsx
@@ -2,17 +2,42 @@ import type {ReactElement} from "react";
 
 export default function Liscensing(): ReactElement {
     return (
-        <pre>
-            I assume we have to link everything we have used?<br/>
-            Refrence the use of:<br/>
-                Flowbite<br/>
-                Flowbite-React<br/>
-                Tailwind CSS<br/>
-                React<br/>
-                NextJS<br/>
-                NodeJS<br/>
-                Docker<br/>
-                ...<br/>
-        </pre>
+        <div>
+            -- Flowbite Components --<br/>
+            Copyright (c) 2023 Bergside Inc.<br/><br/>
+
+            -- Tailwind CSS --<br/>
+            Copyright (c) Tailwind Labs, Inc.<br/><br/>
+
+            -- React --<br/>
+            Copyright (c) Meta Platforms, Inc. and affiliates.<br/><br/>
+
+            -- NextJS --<br/>
+            Copyright (c) 2023 Vercel, Inc.<br/><br/>
+
+            -- NodeJS --<br/>
+            Copyright (c) 2012-2022 by various contributors<br/><br/>
+
+            -- react-quiljs --<br/>
+            Copyright (c) Jehun Seem &#60;jehunseem@gmail.com&#62; (https//github.com/gtgalone)<br/><br/>
+
+            Permission is hereby granted, free of charge, to any person obtaining a copy
+            of this software and associated documentation files (the &quot;Software&quot;), to deal
+            in the Software without restriction, including without limitation the rights
+            to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+            copies of the Software, and to permit persons to whom the Software is
+            furnished to do so, subject to the following conditions:<br/><br/>
+
+            The above copyright notice and this permission notice shall be included in all
+            copies or substantial portions of the Software.<br/><br/>
+
+            THE SOFTWARE IS PROVIDED &quot;AS IS&quot;, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+            IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+            FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+            AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+            LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+            OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+            SOFTWARE.<br/><br/>
+        </div>
     );
 }
