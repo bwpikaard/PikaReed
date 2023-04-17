5476c2daf99ea3389cc5273006351f265c141d07
DrMonotone <calabriley@yahoo.com>
2023-04-17 01:21:07 -0500

    modified table rows to be a pull function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
 import {Carousel} from "flowbite-react";
 import Image from "next/image";
-import Link from "next/link"
+import Link from "next/link";
 import type {ReactElement} from "react";
+
+function tableRows(): ReactElement {
+    const rows: JSX.Element[] = [];
+    for (let i = 0;i < 10;i++) {
+        const oneRow = <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+            <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                <Link href="/novel-homepage">Charlie and the Chocolate Factory</Link>
+            </th>
+            <td className="px-6 py-4">
+                <Link href="/novel-chapter">{`c${10 - i}`}</Link>
+            </td>
+            <td className="px-6 py-4">
+            Roald Dahl
+            </td>
+            <td className="px-6 py-4">
+            192
+            </td>
+        </tr>;
+        rows.push(oneRow);
+    }
+    return <tbody>{rows}</tbody>;
+}
 
 export default function Home(): ReactElement {
     return (
@@ -62,50 +84,7 @@ export default function Home(): ReactElement {
                                 </th>
                             </tr>
                         </thead>
-                        <tbody>
-                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    <Link href="/novel-homepage">Charlie and the Chocolate Factory</Link>
-                                </th>
-                                <td className="px-6 py-4">
-                                    <Link href="/novel-chapter">c23</Link>
-                                </td>
-                                <td className="px-6 py-4">
-                                    Roald Dahl
-                                </td>
-                                <td className="px-6 py-4">
-                                    192
-                                </td>
-                            </tr>
-                            <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    <Link href="#">Wizard of Oz</Link>
-                                </th>
-                                <td className="px-6 py-4">
-                                    <Link href="#">c32</Link>
-                                </td>
-                                <td className="px-6 py-4">
-                                    L. Frank Baum
-                                </td>
-                                <td className="px-6 py-4">
-                                    272
-                                </td>
-                            </tr>
-                            <tr className="bg-white dark:bg-gray-800">
-                                <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                                    <Link href="#">The Alchemist</Link>
-                                </th>
-                                <td className="px-6 py-4">
-                                    <Link href="#">c12</Link>
-                                </td>
-                                <td className="px-6 py-4">
-                                    Paulo Coelho
-                                </td>
-                                <td className="px-6 py-4">
-                                    163
-                                </td>
-                            </tr>
-                        </tbody>
+                        {tableRows()}
                     </table>
                 </div>
             </div>
