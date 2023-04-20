01ea1cc1f5b194b36799448e5000c1c823dcfbe6
DrMonotone <123600653+DrMonotone@users.noreply.github.com>
2023-04-20 01:46:01 -0500

    reversing naming, next auth is picky

--- src/pages/sign-in.tsx
+++ src/pages/sign-in.tsx
@@ -13,8 +13,8 @@ export default function SignIn({csrfToken}: InferGetServerSidePropsType<typeof g
                         <form className="space-y-4 md:space-y-6" method="post" action="/api/auth/callback/credentials">
                             <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
                             <div>
-                                <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
-                                <input type="text" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true} />
+                                <label htmlFor="text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Username</label>
+                                <input type="text" name="username" id="username" className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required={true} />
                             </div>
                             <div>
                                 <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
