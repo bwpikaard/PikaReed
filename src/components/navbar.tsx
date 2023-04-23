b1d76f5204e40eb09538cbc5871531da2da6ba17
DrMonotone <123600653+DrMonotone@users.noreply.github.com>
2023-04-23 14:22:13 -0500

    added role specific tab for navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -15,7 +15,7 @@ export default function Navbar(): ReactElement {
 
     const avaDropRef = createRef<HTMLDivElement>();
     const avaDropButtonRef = createRef<HTMLButtonElement>();
-
+    
     useEffect(() => {
         if (!session?.user || !avaDropRef || !avaDropButtonRef || dropdown) return;
 
@@ -130,6 +130,13 @@ export default function Navbar(): ReactElement {
                     <li>
                         <Link href="/feedback" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Feedback</Link>
                     </li>
+                    {session?.user.roles[0].name !== "mod"
+                        ?   <li>
+                                <Link href="/feedback-list" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Feedback List</Link>
+                            </li>
+                        : <></>
+                    }
+                    
                 </ul>
             </div>
         </div>
