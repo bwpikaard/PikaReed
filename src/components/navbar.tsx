de6bfa39d992f4ad86b62e4b2d502cdfa5e3f9ec
DrMonotone <123600653+DrMonotone@users.noreply.github.com>
2023-04-23 04:24:34 -0500

    added search bar (location is debatable)

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -35,6 +35,16 @@ export default function Navbar(): ReactElement {
                 <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">PikaReed</span>
             </Link>
             <div className="flex items-center md:order-2">
+                <form className="">
+                    <label htmlFor="default-search" className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white ">Search</label>
+                    <div className="relative">
+                        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                            <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+                        </div>
+                        <input type="search" id="default-search" className="block w-96 p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search by Novel..." required />
+                        <button type="submit" className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
+                    </div>
+                </form>
                 {session?.user
                     ? <>
                             <button ref={avaDropButtonRef} id="dropdownAvatarNameButton" data-dropdown-toggle="user-dropdown" className="flex items-center text-sm font-medium text-gray-900 rounded-full hover:text-blue-600 dark:hover:text-blue-500 md:mr-0 dark:text-white" type="button">
@@ -74,7 +84,7 @@ export default function Navbar(): ReactElement {
                             </div>
                         </>
                     : <>
-                            <div className="items-center justify-between w-full md:flex md:w-auto md:order-1">
+                            <div className="items-center justify-between w-full md:flex md:w-auto md:order-1 pl-4">
                                 <ul className="flex flex-col p-1 md:p-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                                     <li>
                                         {/* eslint-disable-next-line @typescript-eslint/no-misused-promises, @typescript-eslint/explicit-function-return-type */}
@@ -94,7 +104,7 @@ export default function Navbar(): ReactElement {
                         <Link href="/" className="block py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Home</Link>
                     </li>
                     <li>
-                        <Link href="#" id="exploreDropdownButton" data-dropdown-toggle="exploreDropdown" data-dropdown-trigger="hover" className="block text-center inline-flex items-center py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
+                        <Link href="/explore/top-ranked" id="exploreDropdownButton" data-dropdown-toggle="exploreDropdown" data-dropdown-trigger="hover" className="block text-center inline-flex items-center py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-gray-400 md:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">
                             Explore <svg className="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 9l-7 7-7-7"></path></svg>
                         </Link>
                         <div id="exploreDropdown" className="z-50 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
